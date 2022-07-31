f7135c79fd006206946423023deb8f5babf59748
GauravUpreti893 <gauravupreti_co20b3_70@dtu.ac.in>
2022-07-31 07:22:20 +0530

    second commit

---
+++ Backend/Routes/Auth.js
@@ -0,0 +1,94 @@
+const { urlencoded } = require('express');
+const express = require('express')
+const User = require('../Models/User');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWTSecret = '@#$!(jt8D%FdFDdj8?::"hn@jfd8(*';
+const fetchUserId = require('../Middleware/fetchUserId');
+router.post('/createuser', [body('email', 'Enter a valid email').isEmail(), body('username', 'Username must be atleast 3 characters long').isLength({ min: 3 }), body('name', 'Name must be atleast 3 characters long').isLength({ min: 3 }), body('password', ' Password must be atleast 5 characters long').isLength({ min: 5 })],
+    async (req, res) => {
+        const errors = validationResult(req);
+        let success = false;
+        if (!errors.isEmpty()) {
+            let e = errors.array();
+            return res.status(400).json({ success, error: e[0].msg });
+        }
+        try {
+            let user = await User.findOne({ username: req.body.username });
+            const er = [];
+            if (user) {
+                er[er.length] = 'Username already exists. Please choose a different Username';
+            }
+            user = await User.findOne({ email: req.body.email });
+            if (user) {
+                er[er.length] = 'Email already exists. Please provide a different Email';
+            }
+            if (er.length) {
+                return res.status(400).json({ success,error: er[0] });
+            }
+            const salt = await bcrypt.genSalt(10);
+            const securepassword = await bcrypt.hash(req.body.password, salt);
+
+            user = await User.create({
+                name: req.body.name,
+                username: req.body.username,
+                password: securepassword,
+                email: req.body.email
+            })
+            const data = {
+                user: user.id
+            }
+            const token = jwt.sign(data, JWTSecret);
+            success = true;
+            res.status(200).json({success, token});
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).json({success, error:'Interanl Server Error'});
+        }
+    });
+router.post('/login', [body('username', 'Invalid Username').isLength({ min: 3 }), body('password', ' Password cannot be blank').exists()],
+    async (req, res) => {
+        const errors = validationResult(req);
+        let success = false;
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success, error: errors.array()[0] });
+        }
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ success, error: 'Invalid Username/Password' });
+        }
+        try {
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(400).json({success, error: 'Invalid Username/Password' });
+            }
+            const data = {
+                user: user.id
+            }
+            const token = jwt.sign(data, JWTSecret)
+            success = true;
+            res.status(200).json({success,token});
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).json({success, error:'Interanl Server Error'});
+        }
+    });
+router.get('/getuser', fetchUserId, async (req, res) => {
+    let success = false;
+    try {
+        let userId = req.user;
+        const user = await User.findById(userId).select("-password");
+        success = true;
+        res.json({success, user});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).json({success, user:'Interanl Server Error'});
+    }
+})
+module.exports = router;
--- Backend/Routes/Notes.js
+++ Backend/Routes/Notes.js
@@ -1,28 +1,123 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const express = require('express');
+const fetchUserId = require('../Middleware/fetchUserId');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const Notes = require('../Models/Notes');
 
-const notesSchema = new Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    title: {
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    tag:{
-        type:String,
-        default:'General'
-    },
-    date: {
-        type:Date,
-        default: Date.now
+router.post('/addnotes', fetchUserId, [
+    body('title', 'Title should be atleast 3 characters long').isLength({ min: 3 }),
+    body('description', 'Description should be atleast 5 characters long').isLength({ min: 5 })
+], async (req, res) => {
+    let success = false;
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({success, error: errors.array()[0] });
+        }
+        const {title, description} = req.body;
+        let tag = req.body.tag;
+        if (tag === '')
+        {
+            tag = 'General';
+        }
+        const note = new Notes({title,description, tag,user:req.user});
+        const saved = await note.save();
+        success = true;
+        res.json({success, saved});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).json({success, error:'Interanl Server Error'});
     }
 });
 
-const Notes = mongoose.model('Notes',notesSchema);
-module.exports =  Notes;
+router.get('/getnotes', fetchUserId, async (req, res) => {
+    let success = false;
+    try {
+        const notes = await Notes.find({user:req.user});
+        success = true;
+        res.json({success, notes});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).json({success, error:'Interanl Server Error'});
+    }
+});
+
+router.put('/updatenotes/:id', [
+    body('title', 'Title should be atleast 3 characters long').isLength({ min: 3 }),
+    body('description', 'Description should be atleast 5 characters long').isLength({ min: 5 })
+],fetchUserId, async (req, res) => {
+    let success = false;
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success, error: errors.array()[0] });
+        }
+        const notesid = req.params.id;
+        if (!notesid)
+        {
+            return res.status(404).json({success, error:'Not Found'});
+        }
+        let notes = await Notes.findById(notesid);
+        if (req.user !== notes.user.toString())
+        {
+            return res.status(401).json({success, error:'Invalid Authentication'});
+        }
+        notes = {};
+        const {title, description} = req.body;
+        let tag = req.body.tag;
+        if (title)
+        {
+            notes.title = title;
+        }
+        if (description)
+        {
+            notes.description = description;
+        }
+        if (tag)
+        {
+            notes.tag = tag;
+        }
+        else
+        {
+            notes.tag = "General";
+        }
+        notes = await Notes.findByIdAndUpdate(notesid,{$set: notes}, {new:true});
+        success = true;
+        res.status(200).json({success, notes});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).json({success, error:'Internal Server Error'});
+    }
+});
+
+router.delete('/deletenotes/:id', fetchUserId, async (req, res) => {
+    let success = false;
+    try {
+        const notesid = req.params.id;
+        if (!notesid)
+        {
+            return res.status(404).json({success, error:'Not Found'});
+        }
+        let notes = await Notes.findById(notesid);
+        if (!notes)
+        {
+            return res.status(404).json({success, error:'Not Found'});
+        }
+        if (req.user !== notes.user.toString())
+        {
+            return res.status(401).json({success, error:'Invalid Authentication'});
+        }
+        notes = await Notes.findByIdAndDelete(notesid);
+        success = true;
+        res.json({success,notes});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).json({success, error:'Interanl Server Error'});
+    }
+});
+
+module.exports = router
