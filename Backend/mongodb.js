d9e1db49f673fe8e2837abd76cb5a52715040213
Gaurav Upreti <76786149+GauravUpreti893@users.noreply.github.com>
2022-07-31 07:27:08 +0530

    Update mongodb.js

--- Backend/mongodb.js
+++ Backend/mongodb.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const MongoUrl = YOUR_MONGODB_URL_HERE;
-// const MongoUrl = 'mongodb+srv://ChatAdmin:Abc%401234@ichatapp.i9eqi.mongodb.net/ChatDatabase?retryWrites=true&w=majority';
 const ConnectToMongo = ()=>{
     mongoose.connect(MongoUrl,()=>{
         console.log('MongoDB Connected');
